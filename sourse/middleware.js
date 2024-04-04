d66b9a84313c89718991869b59e8f8ad7509bc7f
Alex Past <steplerpav@gmail.com>
2024-04-04 17:58:39 +0300

    end JSON BD

--- sourse/middleware.js
+++ sourse/middleware.js
@@ -47,3 +47,11 @@ export function isGuest(req, res, next) {
   else
       next();
 }
+
+export function isLoggedIn(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+}
--- sourse/routes.js
+++ sourse/routes.js
@@ -5,9 +5,9 @@ const { Router, urlencoded, static: staticMiddleware } = express;
 
 import methodOverride from "method-override";
 import { mainPage, detailPage, addPage, add, setDone, remove, setOrder, addendumWrapper } from "./contollers/todos.js"; 
-import { registrPage, register } from "./contollers/users.js";
-import { requestToContext, handlerErrors, extendFlashAPI, getErrors, loadCurrentUser, isGuest } from "./middleware.js";
-import { todoV, registerV } from "./validators.js"; 
+import { registrPage, register, loginPage, login, logout } from "./contollers/users.js";
+import { requestToContext, handlerErrors, extendFlashAPI, getErrors, loadCurrentUser, isGuest, isLoggedIn } from "./middleware.js";
+import { todoV, registerV, loginV } from "./validators.js"; 
 import { mainErrorHandler, error500Handler } from "./error-handlers.js";
 import cookieParser from "cookie-parser";
 import session from "express-session";
@@ -47,10 +47,18 @@ router.use(methodOverride('_method'));
 
 router.use(requestToContext);
 
+// isGuest - Если гость - редиректим на "Главную страницу"
 router.get('/register', isGuest, getErrors, registrPage);
 router.post('/register', isGuest, registerV, handlerErrors, register);
 
+router.get('/login', isGuest, getErrors, loginPage);
+router.post('/login', isGuest, loginV, handlerErrors, login);
+
 router.use(staticMiddleware('public'));
+
+// Проверка вошел ли пользователь в систему
+router.use(isLoggedIn);
+router.post('/logout', logout);
 
 router.get('/add', getErrors, addPage);
 router.post('/add', addendumWrapper, todoV, handlerErrors, add)
